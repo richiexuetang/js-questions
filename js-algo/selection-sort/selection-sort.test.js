5a159f91362b35d46fadf160971c6f29c452df3a
richiexue <49630208+richiexuetang@users.noreply.github.com>
2025-08-19 20:29:23 -0700

    add jest

---
+++ js-algo/selection-sort/selection-sort.test.js
@@ -0,0 +1,16 @@
+const selectionSort = require('./selection-sort');
+
+describe('selectionSort', () => {
+    test('empty', () => {
+        expect(selectionSort([])).toEqual([]);
+    });
+
+    test('one element', () => {
+        expect(selectionSort([1])).toEqual([1]);
+    });
+
+    test('two elements', () => {
+        expect(selectionSort([2, 1])).toEqual([1, 2]);
+        expect(selectionSort([1, 2])).toEqual([1, 2]);
+    });
+});
